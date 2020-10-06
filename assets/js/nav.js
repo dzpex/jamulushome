d2092f8d97afcbe6dab94dacba5cf8ca80e8576c
ann0see <20726856+ann0see@users.noreply.github.com>
2020-10-06 21:56:14 +0200

    Added nav JS and noscript warning

---
+++ assets/js/nav.js
@@ -0,0 +1,8 @@
+function showNav() {
+  var x = document.getElementById("wnv");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
