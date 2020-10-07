c192615fc42cdf5c62ab641af8276fe66746f80c
ann0see <20726856+ann0see@users.noreply.github.com>
2020-10-07 15:26:56 +0200

    Delete

--- _site/es/assets/js/nav.js
+++
@@ -1,8 +0,0 @@
-function showNav() {
-  var x = document.getElementById("wnv");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
-}
